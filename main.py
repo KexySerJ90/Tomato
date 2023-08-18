393957cdc384512f4e46f16db296f1ffdbb4f5e5
KexySerJ90 <exserj9011@gmail.com>
2023-08-18 22:06:59 +0300

    Almost complete

--- main.py
+++ main.py
@@ -6,16 +6,32 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps=0
+timer=None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_canvas,text='00:00')
+    Title_label.config(text='Timer')
+    check.config(text='')
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    countdown(10)
+    global reps
+    reps+=1
+    if reps%2:
+        countdown(WORK_MIN*60)
+        Title_label.config(text='WORK')
+    elif reps==8:
+        countdown(LONG_BREAK_MIN*60)
+        Title_label.config(text='BREAK', fg=RED)
+    else:
+        countdown(SHORT_BREAK_MIN*60)
+        Title_label.config(text='BREAK', fg=PINK)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
     count_min=count//60
@@ -23,11 +39,18 @@ def countdown(count):
     #itemconfig - это метод объекта Canvas в библиотеке tkinter, который позволяет изменять настройки элементов, созданных на холсте. В данном случае, метод itemconfig используется для изменения текста элемента timer_canvas на значение переменной count.
     canvas.itemconfig(timer_canvas,text="{:02d}:{:02d}".format(count_min, count_sec))
     if count > 0:
+        global timer
         #.after() - это метод объекта Tkinter, который позволяет запланировать выполнение функции через определенное количество миллисекунд. Первый аргумент - время задержки в миллисекундах, второй аргумент - функция, которую нужно выполнить после задержки.
-        window.after(1000, countdown, count - 1)
+        timer=window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        mark=""
+        for i in range(reps//2):
+            mark+='✓'
+        check.config(text = mark)
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+        # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title('Pomodoro')
@@ -46,10 +69,10 @@ Title_label.grid(column=1, row=0)
 start = Button(text="Start", highlightthickness=0,command=start_timer)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset", highlightthickness=0)
+reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset.grid(column=2, row=2)
 
-check = Label(text="✓", fg=GREEN, bg=YELLOW)
+check = Label(fg=GREEN, bg=YELLOW)
 check.grid(column=1, row=2)
 
 window.mainloop()
